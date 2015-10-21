5b4f0b665c2f5c4171c71ac1dd0d66f586013fa8
Josh Russell <josh.russell@mobilearth.com>
2015-10-21 16:59:10 -0700

    cleaned up JS file locations

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,28 +132,6 @@ function getAdjective(n){
   return adj;
 }
 
-function getDateString(day, month, year, id){
-  var str = "";
-  var lastDigit = (day % 10);
-  var supStr = "";
-  switch (lastDigit){
-    case 1:
-      supStr = "st";
-      break;
-    case 2:
-      supStr = "nd";
-      break;
-    case 3:
-      supStr = "rd";
-      break;
-    default:
-      supStr = "th";
-      break;
-  }
-
-  str = month + " " + day + supStr.sup() + ", " + year;
-  document.getElementById(id).innerHTML = str;
-}
 
 var multiLingualSubtitle = document.getElementById("multi-lingual-subtitle");
 
