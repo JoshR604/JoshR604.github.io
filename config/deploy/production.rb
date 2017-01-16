26938924d7213375544d14edd31c446756a4cc9c
Joshua Russell <joshrussell91@gmail.com>
2017-01-15 19:50:51 -0800

    production stage configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,4 +60,27 @@
 #     # password: "please use keys"
 #   }
 
+set :stage, :production
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+server '192.34.59.96', user: 'deploy', port: 22, roles: %w{web app}
 
+set :bundle_binstubs, nil
+set :bundle_flags, '--deployment --quiet'
+set :rvm_type, :user
+
+SSHKit.config.command_map[:rake]  = "bundle exec rake"
+SSHKit.config.command_map[:rails] = "bundle exec rails"
+
+namespace :deploy do
+	desc "Restart application"
+	task :update_jekyll do
+		on roles(:app), in: :sequence, wait: 5 do
+  		# execute :touch, release_path.join("tmp/restart.txt")
+		end
+	end
+	after :finishing, "deploy:cleanup"
+end
